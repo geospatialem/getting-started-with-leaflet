26d5387beaa4cbc8f4d2f5876a3a0fa80204897e
geospatialem <geospatialem@gmail.com>
2015-11-11 09:28:29 -0600

    Initial map committed

---
+++ app/script.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([44.95, -93.09], 8);
+
+var OpenStreetMap_BW = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
