1e80838f6f4af91a14196efa942fb3b4ef0b21ce
Kitty Hurley <geospatialem@users.noreply.github.com>
2015-11-12 20:21:43 -0600

    Update script.js
    Add comments

--- app/script.js
+++ app/script.js
@@ -1,6 +1,13 @@
-var map = L.map('map').setView([44.95, -93.09], 8);
+// Initialize the map on the "map" div with a given center and zoom
+var map = L.map('map', {
+    center: [44.95, -93.09], // Centered on St. Paul
+    zoom: 8 // Zoom level
+});
 
+/* Basemap: OpenStreet Map (OSM) Black and White */
+/* Other Leaflet.js Basemaps: http://leaflet-extras.github.io/leaflet-providers/preview */
+/* To add the basemap to the map, use the .addTo(map) call */
 var OpenStreetMap_BW = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-	maxZoom: 18,
+	maxZoom: 18, // The
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
