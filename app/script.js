d8e0513ef4f3ac09003c8e5543864c916379f6ba
Kitty Hurley <geospatialem@users.noreply.github.com>
2015-11-13 21:15:50 -0600

    Update script.js

--- app/script.js
+++ app/script.js
@@ -7,7 +7,11 @@ var map = L.map('map', {
 /* Basemap: OpenStreet Map (OSM) Black and White */
 /* Other Leaflet.js Basemaps: http://leaflet-extras.github.io/leaflet-providers/preview */
 /* To add the basemap to the map, use the .addTo(map) call */
-var OpenStreetMap_BW = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+/*var OpenStreetMap_BW = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18, // The
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);*/
+
+var Thunderforest_SpinalMap = L.tileLayer('https://{s}.tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
