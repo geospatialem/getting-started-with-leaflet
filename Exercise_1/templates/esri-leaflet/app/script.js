0393dd69ac373fad2ff7eb7d68d973cd570b3926
geospatialem <geospatialem@gmail.com>
2016-07-17 13:01:16 -0500

    Adjust urls

---
+++ Exercise_1/templates/esri-leaflet/app/script.js
@@ -0,0 +1,3 @@
+var map = L.map("map").setView([37.75, -122.23], 10);
+
+L.esri.basemapLayer("Topographic").addTo(map);
---
+++ Exercise_1/templates/leaflet/app/script.js
@@ -0,0 +1,12 @@
+// Initialize the map on the "map" div with a given center and zoom
+var map = L.map('map', {
+    center: [44.95, -93.09], // Centered on St. Paul (Lat/Long tool: http://itouchmap.com/latlong.html)
+    zoom: 11 // Zoom level
+});
+
+/* Basemap: Thunderforest Spinal Map */
+/* Other Leaflet.js Basemaps: http://leaflet-extras.github.io/leaflet-providers/preview */
+/* To add the basemap to the map, use the .addTo(map) call */
+var Thunderforest_SpinalMap = L.tileLayer('https://{s}.tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
